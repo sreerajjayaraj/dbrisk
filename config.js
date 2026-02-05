a5b267ad4a547b1ee86c70b8bfc633eef2fe0cfa
sreerajjayaraj <31250143+sreerajjayaraj@users.noreply.github.com>
2026-02-05 21:41:24 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.DB_RISK_DOWNLOAD_URL="https://github.com/sreerajjayaraj/dbrisk/releases/download/v1.0/01_DB-RISK_Product.zip";
+window.DB_RISK_DOWNLOAD_URL = "https://github.com/sreerajjayaraj/dbrisk/releases/download/v1.0/01_DB-RISK_Product.zip";
